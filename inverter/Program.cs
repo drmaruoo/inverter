900e71e30d89fb551065fd408169bdf9cbbe5c92
Marcin Domarski <31709302+drmaruoo@users.noreply.github.com>
2019-12-11 20:58:32 +0100

    Dodaj pliki projektów.

---
+++ inverter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+
+namespace inverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter path to your file (must be png file): ");
+            string path;
+            do
+            {
+                path = GetPath();
+            } while (path == null);
+            Image temp = Image.FromFile(path);
+            Bitmap bmp = new Bitmap(temp);
+            temp.Dispose();
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    bmp.SetPixel(x, y, Color.FromArgb(255-bmp.GetPixel(x,y).R, 255 - bmp.GetPixel(x, y).G, 255 - bmp.GetPixel(x, y).B));
+                }
+            }
+            bmp.Save(path);
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+        }
+        static string GetPath()
+        {
+            string path = Console.ReadLine();
+            if (File.Exists(path)) { return path; } else { Console.WriteLine("Search for file in dir: {0} was unsuccessful. Try again."); return null; }
+        }
+    }
+}
---
+++ inverter/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("inverter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("inverter")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0a628035-ef65-4e94-92d4-6428c0d1dac5")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
